f625d191fdb397e893f08b8e206b23fa1f153119
UESTC-xdd <821378157@qq.com>
2023-10-12 14:51:58 -0400

    first four tasks

--- Assets/Scripts/Components/Interaction/InspectInteractable.cs
+++ Assets/Scripts/Components/Interaction/InspectInteractable.cs
@@ -2,20 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InspectInteractable : IInteractable
 {
+    public Transform RealTrans;
     public bool IsInspecting { get; set; }
 
     public DialogueLine DialogLine;
     public DialogType CurDialogType;
 
+    public UnityEvent OnBeginInteract;
+    public UnityEvent OnEndInteract;
+
     public override void OnInteract()
     {
         base.OnInteract();
         Interactable = false;
-        GameManager.Instance.PlayerInteractor.InspectObj(transform);
+        GameManager.Instance.PlayerInteractor.InspectObj(RealTrans);
         UIMgr.Instance.DialogC.StartDialogue(DialogLine, CurDialogType);
+
+        OnBeginInteract?.Invoke();
 
         EventMgr.OnInteract -= OnEBtn;
         EventMgr.OnInteract += OnEBtn;
@@ -23,6 +30,7 @@ public class InspectInteractable : IInteractable
 
     private void OnEBtn()
     {
+        OnEndInteract?.Invoke();
         UIMgr.Instance.DialogC.StopDialog();
         GameManager.Instance.PlayerInteractor.ReturnInspectObj();
         EventMgr.OnInteract -= OnEBtn;
--- Assets/Scripts/System/Level1Mode.cs
+++ Assets/Scripts/System/Level1Mode.cs
@@ -4,8 +4,77 @@ using UnityEngine;
 
 public class Level1Mode : LevelSingleton<Level1Mode>
 {
+    [Header("First Task")]
+    public Outline CalenderOutline;
+
+    [Header("Second Task")]
+    public Outline ToDoListOutline;
+
+    [Header("Third Task")]
+    public Outline[] AllCleanOutline;
+    public List<IInteractable> AllCleanOutInteractables = new List<IInteractable>();
+
+    private void Start()
+    {
+        SetUpFirstTask();
+    }
+
     public void ChangeToGameMode()
     {
         GameManager.Instance.ChangeToGameMode();
+    }
+
+    public void SetUpFirstTask()
+    {
+        CalenderOutline.enabled = true;
+    }
+
+    public void FinishFirstTask()
+    {
+        Debug.Log("Fist Task Finished");
+        SetUpSecondTask();
+    }
+        
+
+    public void SetUpSecondTask()
+    {
+        ToDoListOutline.enabled = true;
+    }
+
+    public void FinishSecondTask()
+    {
+        Debug.Log("Second Task Finished");
+        SetUpThirdTask();
+    }
+
+    public void SetUpThirdTask()
+    {
+        foreach (var outline in AllCleanOutline)
+        {
+            outline.enabled = true;
+        }
+
+        foreach (var interactable in AllCleanOutInteractables)
+        {
+            interactable.Interactable = true;
+        }
+    }
+
+    public void FinishedOneClean(IInteractable thisInteractable)
+    {
+        if(AllCleanOutInteractables.Contains(thisInteractable))
+        {
+            AllCleanOutInteractables.Remove(thisInteractable);
+            if(AllCleanOutInteractables.Count<=0)
+            {
+                FinishThirdTask();
+            }
+        }
+        thisInteractable.gameObject.SetActive(false);
+    }
+
+    public void FinishThirdTask()
+    {
+        Debug.Log("Third Task Finished");
     }
 }
